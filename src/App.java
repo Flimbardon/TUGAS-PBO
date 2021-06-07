e3c432b2f8dbb6cacd18deb33f736a202e6cf549
Flimbardon <Flimbardon@gmail.com>
2021-06-07 18:14:56 +0700

    Tugas PBO pertemuan 3

--- src/App.java
+++ src/App.java
@@ -1,42 +1,108 @@
 import java.util.Scanner;
  
 public class App {
- 
-    public static void main(String[] args) {
- 
-        Scanner userInput = new Scanner(System.in);
-        float phi = 3.14f;
-        float rusuk, luasP, volume;
- 
-        System.out.println("\t Luas Permukaan & Volume Bola \n");
-        System.out.print(" Masukkan Rusuk : ");
-        rusuk = userInput.nextFloat();
- 
-        luasP = luasPermukaan(rusuk);
-        System.out.println("\n Luas Permukaannya : " + luasP);
- 
-        volume = volumeBola(rusuk);
-        System.out.println("\n Volume Bolanya    : " + volume);
-    }
- 
-    public static float luasPermukaan(float r) {
+    public float luasPermukaan(float r) {
         float phi = 3.14f;
         float hasil;
- 
+
         hasil = 4 * phi * r * r;
         return hasil;
     }
- 
-    public static float volumeBola(float r) {
+
+    public float volumeBola(float r) {
         float phi = 3.14f;
         float hasil;
- 
+
         hasil = 4 / (float) 3 * phi * r * r * r;
         return hasil;
     }
- 
-}public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+
+    public static void main(String[] args) {
+        
+        
+        char choice;
+        do{
+            Scanner userInput = new Scanner(System.in);
+            float rusuk, luasP, volume;
+            
+            App Rusuk = new App();
+
+            System.out.println("\t Luas Permukaan & Volume Bola \n");
+            System.out.print(" Masukkan Rusuk : ");
+            rusuk = userInput.nextFloat();
+        
+            luasP = Rusuk.luasPermukaan(rusuk);
+            System.out.println("\n Luas Permukaannya : " + luasP);
+        
+            volume = Rusuk.volumeBola(rusuk);
+            System.out.println("\n Volume Bolanya    : " + volume);
+            
+            System.out.print("Apakah mau melanjutkan perhitungan? (Y/N)? ");
+
+            Scanner sc = new Scanner(System.in);
+            choice = sc.next().charAt(0);
+
+        }while(choice == 'Y' || choice == 'y');
+    }       
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
